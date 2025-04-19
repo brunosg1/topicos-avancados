3723ca9820520bab07f0a14bf6d93dbcfb164360
vinicius <vinicius@trendmobile.com.br>
2025-04-19 19:13:52 -0300

    removendo arquivos errados

--- api/src/main/java/br/edu/utfpr/api/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.utfpr.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.utfpr.api.model.Pessoa;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-    public Pessoa findByEmail(String nome);
-}
