70b64bddedc597df41d5ed7efce2a172ba8938e3
Vinicius Feliciano <142929633+ViniciusFeliciano12@users.noreply.github.com>
2025-05-07 10:03:29 -0300

    Add files via upload
    add authorization via aws

---
+++ topicos-avancados-main/api/src/main/java/br/edu/utfpr/api/dto/AuthRequest.java
@@ -0,0 +1,11 @@
+package br.edu.utfpr.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthRequest {
+    private String username;
+    private String password;
+}
---
+++ topicos-avancados-main/api/src/main/java/br/edu/utfpr/api/security/InterceptorConfig.java
@@ -0,0 +1,20 @@
+package br.edu.utfpr.api.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private CognitoTokenValidationInterceptor cognitoTokenValidationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(cognitoTokenValidationInterceptor)
+                .addPathPatterns("/culturas/**")
+                .addPathPatterns("/doencas/**"); // Aplica o interceptor aos seus endpoints protegidos
+    }
+}
